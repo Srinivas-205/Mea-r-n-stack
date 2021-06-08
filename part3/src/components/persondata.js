400ef9597c3602e9445558112e5d7a0dd1290872
Srinivasaraju <Srinivas-205@github.com>
2021-06-08 13:39:45 +0530

    Add a file

---
+++ part3/src/components/persondata.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+
+const Form =({handlename,handlenumber,addNote,newName,focusName,Phonenumber}) =>{
+
+    return (
+        <div>
+            <h2>add a new contact</h2>
+            <form onSubmit={addNote}>
+        <div>
+        Name: <input
+          value={newName}
+          onChange={handlename}
+          required autoComplete='off' 
+         ref={focusName}
+        />
+         <div>Number: <input 
+         value={Phonenumber}
+         onChange={handlenumber}
+         required autoComplete='off' 
+         /></div>
+        </div>
+        <div>
+        <button type="submit">add</button>
+        </div>
+      </form> 
+
+ 
+
+        </div>
+    )
+    
+}
+
+export default Form
